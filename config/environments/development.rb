bc1217b30ac7849cc5f02f24ce8c01a89c7cf67b
andrey <litomon275@yandex.ru>
2023-03-24 16:43:31 +0300

    mail test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.default_url_options = { :host => ENV["HOST"] }
-
+=begin
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: '587',
@@ -71,4 +71,5 @@ Rails.application.configure do
     authentication: 'plain',
     enable_starttls_auto: true
   }
+=end
 end
