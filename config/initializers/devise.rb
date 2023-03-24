4f9e395033b12094a6e7c8e7cef78ec9b8b9f3e2
andrey <litomon275@yandex.ru>
2023-03-24 17:12:40 +0300

    mail test2

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'GoodLinks@goodprog-projects.ru'
+  config.mailer_sender = 'ya.ustal@goodprog-projects.ru'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
