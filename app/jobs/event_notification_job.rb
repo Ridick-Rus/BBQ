747b1a54e8bec26d6ac37f97e7dad611604a7bc6
andrey <litomon275@yandex.ru>
2023-03-29 16:15:12 +0300

    fix

--- app/jobs/event_notification_job.rb
+++ app/jobs/event_notification_job.rb
@@ -2,14 +2,14 @@ class EventNotificationJob < ApplicationJob
   queue_as :default
 
   def perform(object, mail = nil)
-    if mail.nil?
+    case when mail.nil?
       all_emails = (object.event.subscriptions.map(&:user_email) + [object.event.user.email]).uniq
       all_emails -= [object.user&.email]
       all_emails.each do |email|
-        EventMailer.try(object.class.to_s.downcase, object, email).deliver_now
+        EventMailer.try(object.class.to_s.downcase, object, email).deliver_later
       end
     else
-      EventMailer.try(object.class.to_s.downcase, object).deliver_now
+      EventMailer.try(object.class.to_s.downcase, object).deliver_later
     end
   end
 end
